5c0b203a20ae8c0b7c74b80348d9188c903fe375
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2024-01-02 11:21:01 +0700

    Add files via upload

---
+++ src/mahasiswa/MahasiswaJava.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author user
+ */
+class Mahasiswa {
+
+    
+    private String nama;
+    private int umur;
+
+    public Mahasiswa(String nama, int umur) {
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }    
+
+    public void displayInfo() {
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);        
+    }
+}
+
+class MahasiswaBaru extends Mahasiswa {
+
+    private int nim;
+
+    public MahasiswaBaru(String nama, int umur, int nim) {
+        super(nama, umur);
+        this.nim = nim;
+    }
+    
+    public int getNim() {
+        return nim;
+    }
+
+    public void setNim(int nim) {
+        this.nim = nim;
+    }
+    
+@Override
+    public void displayInfo() {
+//        System.out.println("Nama: " + nama);
+//        System.out.println("Umur: " + umur);
+        super.displayInfo();
+        System.out.println("NIM: " + nim);
+    }
+}
+
+class MahasiswaLama extends Mahasiswa {
+
+    private int angkatan;
+
+    public MahasiswaLama(String nama, int umur, int angkatan) {
+        super(nama, umur);
+        this.angkatan = angkatan;
+    }
+
+    public int getAngkatan() {
+        return angkatan;
+    }
+
+    public void setAngkatan(int angkatan) {
+        this.angkatan = angkatan;
+    }
+@Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Angkatan: " + angkatan);
+    }
+}
+
+public class MahasiswaJava{
+
+    public static void main(String[] args) {
+        Mahasiswa baru = new MahasiswaBaru("Farchatul", 20, 22205078);
+        baru.displayInfo();
+
+        Mahasiswa lama = new MahasiswaLama("Khirzah", 21, 2022);
+        lama.displayInfo();
+    }
+}
